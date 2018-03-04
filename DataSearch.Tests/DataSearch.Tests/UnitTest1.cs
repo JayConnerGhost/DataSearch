764bb5e2c22aa49e18346e5f2d481e15b11e1f1f
digitalBitch <ghosttk@protonmail.com>
2018-03-04 13:09:39 +0000

    intial commit

---
+++ DataSearch.Tests/DataSearch.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace DataSearch.Tests
+{
+    public class FindBookSpecifications 
+    {
+        [Fact]
+        public void Repository_is_called()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+    }
+}
