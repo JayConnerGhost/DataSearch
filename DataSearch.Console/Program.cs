1282dcfa19279c91d237b6ad7a1ec798947ace5b
digitalBitch <ghosttk@protonmail.com>
2018-03-05 07:27:58 +0000

    Final Commit adding console app to make use of the services + injection container(IOC)

---
+++ DataSearch.Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SqlClient;
+using DataSearch.Models;
+using DataSearch.Repositories;
+using DataSearch.Services;
+using Unity.Lifetime;
+using Unity; 
+    
+namespace DataSearch.Console
+{
+    class Program
+    {
+        private static UnityContainer _container;
+
+        static void Main(string[] args)
+        {
+            Construction();
+            var authorFirstName =PromptForAuthorFirstName();
+            var author = RetreiveAuthor(_container, authorFirstName);
+            author.FirstName = authorFirstName;
+            DisplayAuthorName(author);
+        }
+
+        private static void DisplayAuthorName(IAuthor author)
+        {
+           System.Console.WriteLine(author.LastName+ " "+ author.FirstName);
+            System.Console.ReadKey();
+        }
+
+        private static IAuthor RetreiveAuthor(UnityContainer container, string authorFirstName)
+        {
+          return container.Resolve<IAuthorService>().GetAuthor(authorFirstName);
+        }
+
+        private static string PromptForAuthorFirstName()
+        {
+            System.Console.WriteLine("Please enter Author To Search for");
+            return System.Console.ReadLine();
+        }
+
+        private static void Construction()
+        {
+            _container = new Unity.UnityContainer();
+            var repository= new AuthorRepository(new SqlConnection("Server = localhost; Database = pubs; Trusted_Connection = True;"));
+            _container.RegisterInstance<IAuthorRepository>(repository);
+            _container.RegisterType<IAuthorService, AuthorService>(new PerThreadLifetimeManager());
+        }
+    }
+}
---
+++ DataSearch.Repositories/AuthorRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+using DataSearch.Models;
+
+namespace DataSearch.Repositories
+{
+    public class AuthorRepository:IAuthorRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public AuthorRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+
+        public IAuthor GetAuthor(string authorName)
+        {
+            var authorLastName=string.Empty;
+            _connection.Open();
+            var cmdText = "select * from authors where au_fname='" + authorName + "'";
+            var reader = new SqlCommand(cmdText, _connection).ExecuteReader();
+
+            if (reader.Read())
+            {
+
+                authorLastName = reader.GetFieldValue<string>(1);
+            }
+            
+            _connection.Close();
+
+            return new Author()
+            {
+                LastName = authorLastName,
+            };
+
+        }
+    }
+}
